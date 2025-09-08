429ead43948f82bf5b21ab0e4251d395c23fcabd
Marvin Kosmider <marvin.kosmider@technikum-wien.at>
2025-09-08 14:09:28 +0200

    initial commit

---
+++ src/main/java/at/technikum/server/http/Request.java
@@ -0,0 +1,16 @@
+package at.technikum.server.http;
+
+public class Request {
+
+    private String method;
+
+    private String path;
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+}
